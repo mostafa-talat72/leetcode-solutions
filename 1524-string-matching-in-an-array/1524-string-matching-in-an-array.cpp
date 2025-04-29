cfc34b30e38919b9acb9ba7e87cb0a9baf13e5d1
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-04-29 20:26:13 +0300

    Time: 0 ms (100%), Space: 11.6 MB (36.23%) - LeetHub

---
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+	sort(words.begin(), words.end(), [](string& a, string& b) {return a.size() < b.size(); });
+
+	vector<string> res;
+	for (int i = 0; i < words.size(); i++) {
+		for (int j = i + 1; j < words.size(); j++) {
+			if (words[j].find(words[i]) != string::npos) {
+				res.push_back(words[i]);
+				break;
+			}
+		}
+	}
+	return res;
+
+}
+};
