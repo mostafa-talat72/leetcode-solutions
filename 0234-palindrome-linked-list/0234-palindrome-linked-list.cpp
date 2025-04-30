ccfd1de31d45aca27ea9961282fd303671947819
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-05-01 01:53:21 +0300

    Time: 58 ms (5.81%), Space: 125.7 MB (34.4%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  bool isPalindrome(ListNode* head) {
+	string s = "";
+	while(head){
+		s+=to_string(head->val);
+		head = head->next;
+	}
+	for(int i=0;i<s.size()/2;i++){
+		if (s[i] != s[s.size() - i - 1]) return false;
+	}
+	return true;
+}
+
+};
