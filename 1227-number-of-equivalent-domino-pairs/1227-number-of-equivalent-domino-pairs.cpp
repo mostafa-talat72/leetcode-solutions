359bd98c3241cef76a22a33cb33e56aecc0fe9d7
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-05-04 22:15:04 +0300

    Time: 0 ms (100%), Space: 26.2 MB (74%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) 
 { int n = dominoes.size();
-    for (auto &it :dominoes)
-        sort(it.begin(), it.end());
+    
     int m[10][10] = {0};
     int ans = 0;
     for (int i = 0; i < n; i++)
-        m[dominoes[i][0]][dominoes[i][1]]++;
+        m[min(dominoes[i][0], dominoes[i][1])][max(dominoes[i][0], dominoes[i][1])]++;
     for (int i = 1; i < 10; i++)
         for (int j = 1; j < 10; j++)
             if (m[i][j] > 1)
