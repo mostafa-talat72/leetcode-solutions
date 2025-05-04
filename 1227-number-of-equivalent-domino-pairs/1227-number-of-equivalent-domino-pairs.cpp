1a1a3ecef9fe3f470310c54daa844746e3f0d76f
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-05-04 22:13:54 +0300

    Time: 3 ms (58.5%), Space: 26.2 MB (74%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -4,12 +4,14 @@ public:
 { int n = dominoes.size();
     for (auto &it :dominoes)
         sort(it.begin(), it.end());
-    map<pair<int, int>, int> m;
+    int m[10][10] = {0};
     int ans = 0;
-    for (auto it : dominoes)
-        m[{it[0], it[1]}]++;
-    for (auto it : m)
-        ans += (it.second * (it.second - 1)) / 2;
+    for (int i = 0; i < n; i++)
+        m[dominoes[i][0]][dominoes[i][1]]++;
+    for (int i = 1; i < 10; i++)
+        for (int j = 1; j < 10; j++)
+            if (m[i][j] > 1)
+                ans += (m[i][j] * (m[i][j] - 1)) / 2;
     return ans;
 }
 };
