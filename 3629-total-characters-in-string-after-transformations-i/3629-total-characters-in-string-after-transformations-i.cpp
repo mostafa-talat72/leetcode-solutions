7b427917915d926deb5ed3c83404089b4a9f6ee0
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-05-14 00:09:24 +0300

    Time: 96 ms (75.61%), Space: 19.1 MB (91.22%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
@@ -1,25 +1,85 @@
-const int mod = 1e9 + 7;
-int mod_add(int a, int b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
-
 class Solution {
 public:
-    int lengthAfterTransformations(string s, int t) {
-        int nums[26] = {0};
-        for (char ch : s) nums[ch - 'a']++;
-        while (t--) {
-            int cur[26] = {0};
-            for (int j = 0; j < 26; j++) {
-                if (j == 25 && nums[j] > 0) {
-                    cur[0] = mod_add(cur[0], nums[j]);
-                    cur[1] = mod_add(cur[1], nums[j]);
-                } else {
-                    if (j < 25) cur[j + 1] = mod_add(cur[j + 1], nums[j]);
-                }
-            }
-            for (int j = 0; j < 26; j++) nums[j] = cur[j];
+    vector<int> getRemovableIndices(string str1, string str2)
+{
+    vector<int> ans;   
+    string s1 = "", s2 = "";
+    int n = str1.size();
+    reverse(str1.begin(), str1.end());
+    reverse(str2.begin(), str2.end());
+    char del ='_';
+	 for (int i = n-1; i >= 0 && str2.size(); i--)
+     {
+         if (i != n - 1)
+         {
+             char newCh = del;
+             del = str1.back();
+             str1.pop_back();
+             s1.push_back(newCh);
+             if (s1.size() > s2.size())
+             {
+                 s2.push_back(str2.back());
+                 str2.pop_back();
+             }
+         }else
+         {
+             del = str1.back();
+             str1.pop_back();
+         }
+			
+		 if (str1 == str2 && s1 == s2)
+             ans.push_back(n - i - 1);
+	 }
+     if (ans.empty())
+         ans.push_back(-1);
+     return ans;
+
+}
+int lengthAfterTransformations(string s, int t) {
+    
+
+    int fr[26] = {0}, inc[26] = {0};
+    for (auto it : s)
+        fr[it - 'a']++;
+    int mod = 1e9 + 7;
+    while (t--)
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            fr[i] += inc[i];
+            if (fr[i] >= mod)
+                fr[i] -= mod;
+            inc[i] = 0;
         }
-        int ans = 0;
-        for (int i : nums) ans = mod_add(ans, i);
-        return (int)ans;
+        
+        for (int i = 0; i < 26; i++)
+        {
+            if (i == 25)
+            {
+                inc[0] += fr[i];
+                if (inc[0] >= mod)
+                    inc[0] -= mod;
+                inc[1] += fr[i];
+                if (inc[1] >= mod)
+                    inc[1] -= mod;
+                fr[i] = 0;
+            }
+            else
+            {
+                inc[i + 1] += fr[i];
+                if (inc[i + 1] >= mod)
+                    inc[i + 1] -= mod;
+                fr[i] = 0;
+            }
+        }
     }
+    int ans = 0;
+    for (int i = 0;i < 26;i++)
+    {
+        ans += inc[i];
+        if (ans >= mod)
+            ans -= mod;
+    }
+    return ans;
+}
 };
