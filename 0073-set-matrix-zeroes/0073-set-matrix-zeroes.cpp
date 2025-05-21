2521b60a84948e584f0c21a0a9cc10dd82a17b49
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-05-21 19:18:15 +0300

    Time: 3 ms (11.48%), Space: 19 MB (6.61%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-	unordered_map<int, int> rows, cols;
-	for (int i = 0; i < matrix.size(); i++) {
-		for (int j = 0; j < matrix[0].size(); j++) {
-			if(matrix[i][j] == 0){
-				rows[i] = 1;
-				cols[j] = 1;
-			}
-		}
-	}
+    
 
-	for (int i = 0; i < matrix.size(); i++) {
-		for (int j = 0; j < matrix[0].size(); j++) {
-			if (rows[i] || cols[j]) {
-				matrix[i][j] = 0;
-			}
-		}
-	}
+    set<int> row, column;
+    for (int i = 0; i < matrix.size();i++)
+    {
+        for (int j = 0; j < matrix[0].size(); j++)
+        {
+            if (!matrix[i][j])
+            {
+                row.insert(i);
+                column.insert(j);
+            }
+        }
+    }
+    for (auto it : row)
+    {
+        for (int i = 0; i < matrix[0].size(); i++)
+            matrix[it][i] = 0;
+    }
+    for (auto it : column)
+    {
+        for (int i = 0; i < matrix.size(); i++)
+            matrix[i][it] = 0;
+    }
+
 }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
