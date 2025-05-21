121697a79e65649063a781bac937fdc8da7c0d38
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-05-21 21:15:54 +0300

    Time: 0 ms (100%), Space: 11.6 MB (49.22%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-   void sortColors(vector<int>& nums) {
-
-	int zero=count(nums.begin(),nums.end(),0);
-	int one=count(nums.begin(),nums.end(),1);
-	int two=count(nums.begin(),nums.end(),2);
-	for (int i = 0; i < zero; i++)
-		nums[i] = 0;
-	for (int i = zero; i < zero + one; i++)
-		nums[i] =1;
-	for (int i = zero + one; i < nums.size(); i++)
-		nums[i] = 2;
+    void sortColors(vector<int>& nums) { 
+   int cnt[3] = {0};
+    for (auto it : nums)
+        cnt[it]++;
+    
+   fill(nums.begin(), nums.begin() + cnt[0], 0);
+    fill(nums.begin() + cnt[0], nums.begin() + cnt[0] + cnt[1], 1);
+    fill(nums.begin() + cnt[0] + cnt[1],nums.end(), 2);
 }
 };
