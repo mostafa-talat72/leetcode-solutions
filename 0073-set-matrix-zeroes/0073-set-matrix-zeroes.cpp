9c7b71a34b108150aa46d43f9e30d0e2c793cf74
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-05-21 19:35:28 +0300

    Time: 0 ms (100%), Space: 18.7 MB (24.6%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-    
-
-    set<int> row, column;
+    unordered_set<int> row, column;
     for (int i = 0; i < matrix.size();i++)
     {
         for (int j = 0; j < matrix[0].size(); j++)
@@ -25,6 +23,5 @@ public:
         for (int i = 0; i < matrix.size(); i++)
             matrix[i][it] = 0;
     }
-
 }
 };
