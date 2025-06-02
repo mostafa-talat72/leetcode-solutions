e11acaad4f299d33e03cc7c3942a90daf1697f95
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-06-02 15:18:59 +0300

    Time: 17 ms (8.39%), Space: 25.7 MB (5.06%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,54 +1,51 @@
 class Solution {
 public:
-     int candy(vector<int>& ratings) { 
-	 
-	 priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    int candy(vector<int>& ratings) { 
+ 
+ priority_queue<pair<int, int>> pq;
 
-	 int n = ratings.size();
-     vector<int> candies(n, 1);
-     for (int i = 0; i < n; i++)
-     {
-		 pq.push({ratings[i], i});
-     }
+ int n = ratings.size();
+    vector<int> candies(n, 1);
+    for (int i = 0; i < n; i++)
+    {
+        ratings[i]++;
+	 pq.push({-ratings[i] , i});
+    }
+    int ans = 0;
 
-	 while (!pq.empty())
-     {
-         auto [rating, index] = pq.top();
-         pq.pop();
-         rating = rating;
-         if (index == 0)
-         {
-             if (index+1<n && ratings[index] > ratings[index + 1])
-			 {
-				 candies[index] = candies[index + 1] + 1;
-             }
-         }
-         else if (index == n - 1)
-         {
-             if (ratings[index] > ratings[index - 1])
-                 candies[index] = candies[index - 1] + 1;
-         }
-		 else {
-             if (rating > ratings[index - 1] && rating > ratings[index + 1])
-			 {
-				 candies[index] = max(candies[index - 1], candies[index + 1]) + 1;
-			 }
-			 else if (rating > ratings[index - 1])
-			 {
-				 candies[index] = candies[index - 1] + 1;
-			 }
-			 else if (rating > ratings[index + 1])
-			 {
-				 candies[index] = candies[index + 1] + 1;
-             }
+ while (!pq.empty())
+    {
+        auto [rating, index] = pq.top();
+        pq.pop();
+        rating = -rating;
+        if (index == 0)
+        {
+            if (index+1<n && ratings[index] > ratings[index + 1])
+		 {
+			 candies[index] = candies[index + 1] + 1;
+            }
+        }
+        else if (index == n - 1)
+        {
+            if (ratings[index] > ratings[index - 1])
+                candies[index] = candies[index - 1] + 1;
+        }
+	 else {
+            if (rating > ratings[index - 1] && rating > ratings[index + 1])
+		 {
+			 candies[index] = max(candies[index - 1], candies[index + 1]) + 1;
 		 }
-     }
-
-	 int ans = 0;
-	 for (int i = 0; i < n; i++)
-	 {
-		 ans += candies[i];
-     }
-     return ans;
- }
+		 else if (rating > ratings[index - 1])
+		 {
+			 candies[index] = candies[index - 1] + 1;
+		 }
+		 else if (rating > ratings[index + 1])
+		 {
+			 candies[index] = candies[index + 1] + 1;
+            }
+	 }
+        ans += candies[index];
+    }
+    return ans;
+}
 };
