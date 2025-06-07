7ea50d81aab568c449fc02808c84067954a9d56d
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-06-07 11:37:04 +0300

    Time: 264 ms (23.37%), Space: 108.8 MB (16.56%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,8 +1,22 @@
+struct Compare
+{
+    bool operator()(pair<char, int> a, pair<char, int> b)
+    {
+        if (a.first < b.first)
+            return true;
+        else if (a.first > b.first)
+            return false;
+        else
+            return a.second > b.second;
+    }
+};
 class Solution {
 public:
-    string clearStars(string s) { 
+    string clearStars(string s)
+{ 
 
 	map<char, vector<int>> mp;
+    priority_queue<pair<char, int>, vector<pair<char, int>>, Compare> pq;
     int n = s.size();
     for (int i=0;i<s.size();i++) {
 		if (s[i] != '*') {
