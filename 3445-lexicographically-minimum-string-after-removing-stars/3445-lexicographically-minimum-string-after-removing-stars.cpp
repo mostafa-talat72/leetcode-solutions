7a6d0d1c80a63c15d462ad59cb08ca1fa7f4acde
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-06-07 11:39:46 +0300

    Time: 111 ms (64.28%), Space: 25.5 MB (83.77%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -2,12 +2,12 @@ struct Compare
 {
     bool operator()(pair<char, int> a, pair<char, int> b)
     {
-        if (a.first < b.first)
+        if (a.first > b.first)
             return true;
-        else if (a.first > b.first)
+        else if (a.first < b.first)
             return false;
         else
-            return a.second > b.second;
+            return a.second < b.second;
     }
 };
 class Solution {
@@ -15,21 +15,17 @@ public:
     string clearStars(string s)
 { 
 
-	map<char, vector<int>> mp;
     priority_queue<pair<char, int>, vector<pair<char, int>>, Compare> pq;
     int n = s.size();
     for (int i=0;i<s.size();i++) {
 		if (s[i] != '*') {
-			mp[s[i]].push_back(i);
+            pq.push({s[i], i});
         }
         else
         {
             s[i] = ' ';
-            s[mp.begin()->second.back()] = ' ';
-            mp.begin()->second.pop_back();
-            if (mp.begin()->second.empty()) {
-				mp.erase(mp.begin());
-            }
+            s[pq.top().second] = ' ';
+            pq.pop();
         }
     }
     s.erase(remove(s.begin(), s.end(), ' '), s.end());
