f57c30d37dbb2fd43790bc355954da5f68da05f0
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-06-08 03:27:43 +0300

    Time: 443 ms (5.24%), Space: 15.8 MB (22.65%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,6 +1,6 @@
 bool compare(int a,int b)
 { 
-	return to_string(a)< to_string(b);
+	return to_string(a) < to_string(b);
 }
 class Solution {
 public:
@@ -9,7 +9,10 @@ public:
 vector<int> lexicalOrder(int n) { 
 	
 	vector<int> ans(n); 
-	iota(ans.begin(), ans.end(), 1);
+	for (int i = 0; i < n; i++)
+    {
+        ans[i] = i + 1;
+    }
     sort(ans.begin(), ans.end(), compare);
     return ans;
 }
