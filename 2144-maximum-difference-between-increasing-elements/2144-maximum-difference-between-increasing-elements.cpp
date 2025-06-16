fcde11d3e00f51e04b6510904aa9dcc462f35a1b
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-06-16 19:43:21 +0300

    Time: 0 ms (100%), Space: 12 MB (38.11%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -6,7 +6,6 @@ public:
 	int ans = -1, mxNum = nums.back(); 
 	for (int i = nums.size() - 2; i >= 0; i--)
     {
-        cout << nums[i] << " " << mxNum << " " << mxNum - nums[i] << endl;
         if (mxNum == nums[i])
             continue; 
 		ans = max(ans, mxNum - nums[i]);
