bef4cee2e21c55d4c453d57ccbd73193776f725b
Mostafa TaL3t <51420393+mostafa-talat72@users.noreply.github.com>
2025-07-07 02:52:51 +0300

    Time: 79 ms (78.99%), Space: 81.9 MB (88.13%) - LeetHub

--- 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.cpp
@@ -1,28 +1,44 @@
-class FindSumPairs {
+class FindSumPairs
+{
 private:
-    vector<int> n1, n2;
-    unordered_map<int, int> m;
+    unordered_map<int, int> mp;
+    vector<int> nums2, nums1;
 
 public:
-    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
-        n1 = nums1;
-        n2 = nums2;
-        for (int x : n2) {
-            m[x]++;
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) { 
+		mp.clear();
+		for (int i = 0; i < nums2.size(); i++) {
+			mp[nums2[i]]++;
         }
-    }
+        this->nums1 = nums1;
+        this->nums2 = nums2;
+	}
 
-    void add(int i, int v) {
-        m[n2[i]]--;
-        n2[i] += v;
-        m[n2[i]]++;
-    }
-
-    int count(int t) {
-        int c = 0;
-        for (int x : n1) {
-            c += m[t - x];
+    void add(int index, int val) { 
+		mp[nums2[index]]--;
+        if (mp[nums2[index]] == 0) {
+			mp.erase(nums2[index]);
         }
-        return c;
-    }
+        mp[nums2[index] + val]++;
+        nums2[index] += val;
+	}
+
+    int count(int tot) { 
+		int ret = 0;
+		for (auto& it : nums1) {
+            if (tot > it && mp.find(tot - it) != mp.end())
+            {
+                ret += mp[tot - it];
+            }
+        }
+        return ret ;
+	}
+
 };
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+ * obj->add(index,val);
+ * int param_2 = obj->count(tot);
+ */
